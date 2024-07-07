a427fbca5f61fb6a5bbbb7919803f28f87009be8
eliud <githukueliud@gmail.com>
2024-07-07 23:30:48 +0300

    production tests

---
+++ src/test/java/com/example/hng_stage2_backend/auth/JwtTests.java
@@ -0,0 +1,4 @@
+package com.example.hng_stage2_backend.auth;
+
+public class JwtTests {
+}
--- src/test/java/com/example/hng_stage2_backend/auth/OrganizationTests.java
+++ src/test/java/com/example/hng_stage2_backend/auth/OrganizationTests.java
@@ -1,48 +1,48 @@
 package com.example.hng_stage2_backend.auth;
 
-//
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.http.MediaType;
-//import org.springframework.test.web.servlet.MockMvc;
-//import org.springframework.test.web.servlet.ResultActions;
-//
-//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-//
-//
-//@SpringBootTest
-//@AutoConfigureMockMvc
-//public class OrganizationTests {
-//    @Autowired
-//    private MockMvc mockMvc;
-//
-//    //ascertains that the user must be logged in to get the organizations
-////    @Test
-////    void testGetUserOrganizationsRequiresAuthentication() throws Exception {
-////        mockMvc.perform(get("/api/organisations")
-////                        .contentType(MediaType.APPLICATION_JSON))
-////                .andExpect(status().isUnauthorized())
-////                .andExpect(jsonPath("$.status").doesNotExist())
-////                .andExpect(jsonPath("$.message").doesNotExist())
-////                .andExpect(jsonPath("$.data").doesNotExist());
-////    }
-//
-//
-////    @Test
-////    void testCreateOrganizationRequiresAuthentication() throws Exception {
-////        String requestBody = "{\"name\":\"New Organization\",\"description\":\"New Organization Description\"}";
-////
-////        mockMvc.perform(post("/api/organisations")
-////                        .contentType(MediaType.APPLICATION_JSON)
-////                        .content(requestBody))
-////                .andExpect(status().isUnauthorized())
-////                .andExpect(jsonPath("$.status").doesNotExist()) // No status field expected
-////                .andExpect(jsonPath("$.message").doesNotExist()) // No message field expected
-////                .andExpect(jsonPath("$.data").doesNotExist()); // No data field expected
-////    }
-//}
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class OrganizationTests {
+    @Autowired
+    private MockMvc mockMvc;
+
+    //ascertains that the user must be logged in to get the organizations
+    @Test
+    void testGetUserOrganizationsRequiresAuthentication() throws Exception {
+        mockMvc.perform(get("/api/organisations")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isUnauthorized())
+                .andExpect(jsonPath("$.status").doesNotExist())
+                .andExpect(jsonPath("$.message").doesNotExist())
+                .andExpect(jsonPath("$.data").doesNotExist());
+    }
+
+
+    @Test
+    void testCreateOrganizationRequiresAuthentication() throws Exception {
+        String requestBody = "{\"name\":\"New Organization\",\"description\":\"New Organization Description\"}";
+
+        mockMvc.perform(post("/api/organisations")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestBody))
+                .andExpect(status().isUnauthorized())
+                .andExpect(jsonPath("$.status").doesNotExist()) // No status field expected
+                .andExpect(jsonPath("$.message").doesNotExist()) // No message field expected
+                .andExpect(jsonPath("$.data").doesNotExist()); // No data field expected
+    }
+}
