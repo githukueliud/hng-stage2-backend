6a69faac35582bb311d7d9b15f35d8cf0793e48d
eliud <githukueliud@gmail.com>
2024-07-07 20:29:55 +0300

    organization tests

--- src/test/java/com/example/hng_stage2_backend/auth/AuthenticationTests.java
+++ src/test/java/com/example/hng_stage2_backend/auth/AuthenticationTests.java
@@ -1,124 +1,124 @@
 package com.example.hng_stage2_backend.auth;
-
-import com.example.hng_stage2_backend.auth.response.SuccessAuthResponse;
-import com.example.hng_stage2_backend.user.entity.User;
-import com.example.hng_stage2_backend.user.repository.UserRepository;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.http.MediaType;
-
-import java.util.UUID;
-
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.hamcrest.Matchers.notNullValue;
-import static org.mockito.ArgumentMatchers.startsWith;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-
-
-@SpringBootTest
-@AutoConfigureMockMvc
-@ExtendWith(MockitoExtension.class)
-public class AuthenticationTests {
-
-    @Autowired
-    private MockMvc mockMvc;
-
-    @Autowired
-    private ObjectMapper objectMapper;
-
-    @Mock
-    private AuthenticationService authService;
-
-    @InjectMocks
-    private AuthenticationController authController;
-
-
-
-    @Autowired
-    private UserRepository userRepository;
-
-    @Autowired
-    private PasswordEncoder passwordEncoder;
-
-    @BeforeEach
-    public void setUp() {
-        // Ensure the user exists in the repository
-        User user = new User();
-        user.setId(UUID.randomUUID());
-        user.setEmail("john.doe@example.com");
-        user.setPassword(passwordEncoder.encode("password")); // Use BCryptPasswordEncoder to encode the password
-        user.setFirstName("John");
-        user.setLastName("Doe");
-        user.setPhone("1234567890");
-
-        userRepository.save(user);
-    }
-
-//    @Test
-//    public void testThatUserIsLoggedInSuccessfully() throws Exception {
-//        // Setup request data
-//        LoginForm loginForm = new LoginForm("john.doe@example.com", "password");
 //
-//        // Perform the login request
-//        mockMvc.perform(post("/auth/login")
-//                        .contentType(MediaType.APPLICATION_JSON)
-//                        .content(asJsonString(loginForm)))
-//                .andExpect(status().isOk())
-//                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-//                // Validate the static parts of the response
-//                .andExpect(jsonPath("$.status").value("success"))
-//                .andExpect(jsonPath("$.message").value("Login successful"))
-//                .andExpect(jsonPath("$.data.user.email").value("john.doe@example.com"))
-//                .andExpect(jsonPath("$.data.user.firstName").value("John"))
-//                .andExpect(jsonPath("$.data.user.lastName").value("Doe"))
-//                .andExpect(jsonPath("$.data.user.phone").value("1234567890"))
-//                // Validate the dynamic parts (e.g., check if accessToken and userId are non-null and valid)
-//                .andExpect(jsonPath("$.data.accessToken").isNotEmpty())
-//                .andExpect(jsonPath("$.data.user.userId").isNotEmpty());
-//    }
-
-    // Utility method to convert object to JSON string
-//    public static String asJsonString(final Object obj) {
-//        try {
-//            return new ObjectMapper().writeValueAsString(obj);
-//        } catch (Exception e) {
-//            throw new RuntimeException(e);
-//        }
-//    }
-
-
-
-//    @Test
-//    public void testThatUserLoginFails() throws Exception {
-//        // Create login request
-//        AuthenticationRequest loginRequest = new AuthenticationRequest();
-//        loginRequest.setEmail("wrong.email@example.com");
-//        loginRequest.setPassword("wrongpassword");
+//import com.example.hng_stage2_backend.auth.response.SuccessAuthResponse;
+//import com.example.hng_stage2_backend.user.entity.User;
+//import com.example.hng_stage2_backend.user.repository.UserRepository;
+//import com.fasterxml.jackson.databind.ObjectMapper;
+//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.extension.ExtendWith;
+//import org.mockito.InjectMocks;
+//import org.mockito.Mock;
+//import org.mockito.junit.jupiter.MockitoExtension;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//import org.springframework.test.web.servlet.MockMvc;
+//import org.springframework.http.MediaType;
 //
-//        // Expected response
-//        String expectedResponse = "{\"status\":\"Bad Request\",\"message\":\"Authentication failed\",\"statusCode\":401}";
+//import java.util.UUID;
 //
-//        mockMvc.perform(post("/auth/login")
-//                        .contentType(MediaType.APPLICATION_JSON)
-//                        .content(objectMapper.writeValueAsString(loginRequest)))
-//                .andExpect(status().isBadRequest())
-//                .andExpect(content().json(expectedResponse));
+//
+//import static org.assertj.core.api.Assertions.assertThat;
+//import static org.hamcrest.Matchers.notNullValue;
+//import static org.mockito.ArgumentMatchers.startsWith;
+//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+//
+//
+//@SpringBootTest
+//@AutoConfigureMockMvc
+//@ExtendWith(MockitoExtension.class)
+//public class AuthenticationTests {
+//
+//    @Autowired
+//    private MockMvc mockMvc;
+//
+//    @Autowired
+//    private ObjectMapper objectMapper;
+//
+//    @Mock
+//    private AuthenticationService authService;
+//
+//    @InjectMocks
+//    private AuthenticationController authController;
+//
+//
+//
+//    @Autowired
+//    private UserRepository userRepository;
+//
+//    @Autowired
+//    private PasswordEncoder passwordEncoder;
+//
+//    @BeforeEach
+//    public void setUp() {
+//        // Ensure the user exists in the repository
+//        User user = new User();
+//        user.setId(UUID.randomUUID());
+//        user.setEmail("john.doe@example.com");
+//        user.setPassword(passwordEncoder.encode("password")); // Use BCryptPasswordEncoder to encode the password
+//        user.setFirstName("John");
+//        user.setLastName("Doe");
+//        user.setPhone("1234567890");
+//
+//        userRepository.save(user);
 //    }
-
-
-
-
-}
+//
+////    @Test
+////    public void testThatUserIsLoggedInSuccessfully() throws Exception {
+////        // Setup request data
+////        LoginForm loginForm = new LoginForm("john.doe@example.com", "password");
+////
+////        // Perform the login request
+////        mockMvc.perform(post("/auth/login")
+////                        .contentType(MediaType.APPLICATION_JSON)
+////                        .content(asJsonString(loginForm)))
+////                .andExpect(status().isOk())
+////                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+////                // Validate the static parts of the response
+////                .andExpect(jsonPath("$.status").value("success"))
+////                .andExpect(jsonPath("$.message").value("Login successful"))
+////                .andExpect(jsonPath("$.data.user.email").value("john.doe@example.com"))
+////                .andExpect(jsonPath("$.data.user.firstName").value("John"))
+////                .andExpect(jsonPath("$.data.user.lastName").value("Doe"))
+////                .andExpect(jsonPath("$.data.user.phone").value("1234567890"))
+////                // Validate the dynamic parts (e.g., check if accessToken and userId are non-null and valid)
+////                .andExpect(jsonPath("$.data.accessToken").isNotEmpty())
+////                .andExpect(jsonPath("$.data.user.userId").isNotEmpty());
+////    }
+//
+//    // Utility method to convert object to JSON string
+////    public static String asJsonString(final Object obj) {
+////        try {
+////            return new ObjectMapper().writeValueAsString(obj);
+////        } catch (Exception e) {
+////            throw new RuntimeException(e);
+////        }
+////    }
+//
+//
+//
+////    @Test
+////    public void testThatUserLoginFails() throws Exception {
+////        // Create login request
+////        AuthenticationRequest loginRequest = new AuthenticationRequest();
+////        loginRequest.setEmail("wrong.email@example.com");
+////        loginRequest.setPassword("wrongpassword");
+////
+////        // Expected response
+////        String expectedResponse = "{\"status\":\"Bad Request\",\"message\":\"Authentication failed\",\"statusCode\":401}";
+////
+////        mockMvc.perform(post("/auth/login")
+////                        .contentType(MediaType.APPLICATION_JSON)
+////                        .content(objectMapper.writeValueAsString(loginRequest)))
+////                .andExpect(status().isBadRequest())
+////                .andExpect(content().json(expectedResponse));
+////    }
+//
+//
+//
+//
+//}
--- src/test/java/com/example/hng_stage2_backend/auth/RegistrationTests.java
+++ src/test/java/com/example/hng_stage2_backend/auth/RegistrationTests.java
@@ -1,151 +1,151 @@
 package com.example.hng_stage2_backend.auth;
 
-
-import com.example.hng_stage2_backend.config.JwtService;
-import com.example.hng_stage2_backend.organization.entity.Organization;
-import com.example.hng_stage2_backend.organization.repository.OrganizationRepository;
-import com.example.hng_stage2_backend.user.entity.User;
-import com.example.hng_stage2_backend.user.repository.UserRepository;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.ResultMatcher;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-
-import java.util.Collections;
-import java.util.UUID;
-
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-@SpringBootTest
-@AutoConfigureMockMvc
-public class RegistrationTests {
-
-    @Autowired
-    private ObjectMapper objectMapper;
-
-    @Autowired
-    private MockMvc mockMvc;
-
-    @MockBean
-    private UserRepository userRepository;
-
-    @MockBean
-    private OrganizationRepository organizationRepository;
-
-    @MockBean
-    private JwtService jwtService;
-
-    @MockBean
-    private PasswordEncoder passwordEncoder;
-
-    @BeforeEach
-    void setUp() {
-        Mockito.when(passwordEncoder.encode(Mockito.anyString())).thenReturn("encodedPassword");
-    }
-
-//    @Test
-//    void testRegisterUserSuccessfullyWithDefaultOrganization() throws Exception {
-//        // Arrange
-//        RegisterRequest request = new RegisterRequest();
-//        request.setFirstName("John");
-//        request.setLastName("Doe");
-//        request.setEmail("john.doe@example.com");
-//        request.setPassword("password");
-//        request.setPhone("1234567890");
 //
-//        // Mock repository behavior
-//        Mockito.when(userRepository.save(Mockito.any())).thenReturn(new User());
+//import com.example.hng_stage2_backend.config.JwtService;
+//import com.example.hng_stage2_backend.organization.entity.Organization;
+//import com.example.hng_stage2_backend.organization.repository.OrganizationRepository;
+//import com.example.hng_stage2_backend.user.entity.User;
+//import com.example.hng_stage2_backend.user.repository.UserRepository;
+//import com.fasterxml.jackson.databind.ObjectMapper;
+//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.Test;
+//import org.mockito.Mockito;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.boot.test.mock.mockito.MockBean;
+//import org.springframework.http.MediaType;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//import org.springframework.test.web.servlet.MockMvc;
+//import org.springframework.test.web.servlet.MvcResult;
+//import org.springframework.test.web.servlet.ResultMatcher;
+//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 //
-//        // Act & Assert
-//        mockMvc.perform(post("/auth/register")
-//                        .contentType(MediaType.APPLICATION_JSON)
-//                        .content(objectMapper.writeValueAsString(request)))
-//                .andExpect(status().isBadRequest())
-//                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-//                .andExpect(jsonPath("$.status").value("Bad Request"))
-//                .andExpect(jsonPath("$.message").value("Registration unsuccessful"))
-//                .andExpect(jsonPath("$.statusCode").value(400));
+//import java.util.Collections;
+//import java.util.UUID;
+//
+//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+//
+//@SpringBootTest
+//@AutoConfigureMockMvc
+//public class RegistrationTests {
+//
+//    @Autowired
+//    private ObjectMapper objectMapper;
+//
+//    @Autowired
+//    private MockMvc mockMvc;
+//
+//    @MockBean
+//    private UserRepository userRepository;
+//
+//    @MockBean
+//    private OrganizationRepository organizationRepository;
+//
+//    @MockBean
+//    private JwtService jwtService;
+//
+//    @MockBean
+//    private PasswordEncoder passwordEncoder;
+//
+//    @BeforeEach
+//    void setUp() {
+//        Mockito.when(passwordEncoder.encode(Mockito.anyString())).thenReturn("encodedPassword");
 //    }
-
-
-//    @Test
-//    void testLoginUserSuccessfully() throws Exception {
-//        // Arrange
-//        String email = "john.doe@example.com";
-//        String password = "password";
-//        String encodedPassword = "encodedPassword";
 //
-//        // Create a mock User entity with an ID
-//        User user = new User();
-//        user.setId(UUID.randomUUID()); // Set a random UUID for the ID
-//        user.setEmail(email);
-//        user.setPassword(encodedPassword);
+////    @Test
+////    void testRegisterUserSuccessfullyWithDefaultOrganization() throws Exception {
+////        // Arrange
+////        RegisterRequest request = new RegisterRequest();
+////        request.setFirstName("John");
+////        request.setLastName("Doe");
+////        request.setEmail("john.doe@example.com");
+////        request.setPassword("password");
+////        request.setPhone("1234567890");
+////
+////        // Mock repository behavior
+////        Mockito.when(userRepository.save(Mockito.any())).thenReturn(new User());
+////
+////        // Act & Assert
+////        mockMvc.perform(post("/auth/register")
+////                        .contentType(MediaType.APPLICATION_JSON)
+////                        .content(objectMapper.writeValueAsString(request)))
+////                .andExpect(status().isBadRequest())
+////                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+////                .andExpect(jsonPath("$.status").value("Bad Request"))
+////                .andExpect(jsonPath("$.message").value("Registration unsuccessful"))
+////                .andExpect(jsonPath("$.statusCode").value(400));
+////    }
 //
-//        // Mock repository behavior
-//        Mockito.when(userRepository.findUserByEmail(email)).thenReturn(java.util.Optional.of(user));
-//        Mockito.when(passwordEncoder.matches(password, encodedPassword)).thenReturn(true);
 //
-//        // Mock JWT token generation
-//        String token = "mockedJwtToken";
-//        Mockito.when(jwtService.generateToken(Mockito.any())).thenReturn(token);
+////    @Test
+////    void testLoginUserSuccessfully() throws Exception {
+////        // Arrange
+////        String email = "john.doe@example.com";
+////        String password = "password";
+////        String encodedPassword = "encodedPassword";
+////
+////        // Create a mock User entity with an ID
+////        User user = new User();
+////        user.setId(UUID.randomUUID()); // Set a random UUID for the ID
+////        user.setEmail(email);
+////        user.setPassword(encodedPassword);
+////
+////        // Mock repository behavior
+////        Mockito.when(userRepository.findUserByEmail(email)).thenReturn(java.util.Optional.of(user));
+////        Mockito.when(passwordEncoder.matches(password, encodedPassword)).thenReturn(true);
+////
+////        // Mock JWT token generation
+////        String token = "mockedJwtToken";
+////        Mockito.when(jwtService.generateToken(Mockito.any())).thenReturn(token);
+////
+////        // Act & Assert
+////        mockMvc.perform(post("/auth/login")
+////                        .contentType(MediaType.APPLICATION_JSON)
+////                        .content("{\"email\":\"john.doe@example.com\",\"password\":\"password\"}"))
+////                .andExpect(status().isOk())
+////                .andExpect(jsonPath("$.status").value("success"))
+////                .andExpect(jsonPath("$.message").value("Login successful"))
+////                .andExpect(jsonPath("$.data.accessToken").value(token)) //accessToken
+////                .andExpect(jsonPath("$.data.user.email").value(email));
+////    }
 //
-//        // Act & Assert
-//        mockMvc.perform(post("/auth/login")
-//                        .contentType(MediaType.APPLICATION_JSON)
-//                        .content("{\"email\":\"john.doe@example.com\",\"password\":\"password\"}"))
-//                .andExpect(status().isOk())
-//                .andExpect(jsonPath("$.status").value("success"))
-//                .andExpect(jsonPath("$.message").value("Login successful"))
-//                .andExpect(jsonPath("$.data.accessToken").value(token)) //accessToken
-//                .andExpect(jsonPath("$.data.user.email").value(email));
-//    }
-
-
-
-
-//    @Test
-//    void testRegisterUserFailsIfRequiredFieldsAreMissing() throws Exception {
-//        RegisterRequest request = new RegisterRequest();
-//        request.setEmail("john.doe@example.com");
-//        request.setPassword("password");
-//        request.setPhone("1234567890");
 //
-//        mockMvc.perform(post("/auth/register")
-//                        .contentType(MediaType.APPLICATION_JSON)
-//                        .content(objectMapper.writeValueAsString(request)))
-//                .andExpect(status().isBadRequest())
-//                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-//                .andExpect(jsonPath("$.status").value("Bad Request"))
-//                .andExpect(jsonPath("$.message").value("Registration unsuccessful"))
-//                .andExpect(jsonPath("$.statusCode").value(400));
-//    }
-
-//    @Test
-//    void testRegisterUserFailsIfDuplicateEmail() throws Exception {
-//        // Arrange
-//        User existingUser = new User();
-//        existingUser.setEmail("john.doe@example.com");
-//        Mockito.when(userRepository.findUserByEmail("john.doe@example.com")).thenReturn(java.util.Optional.of(existingUser));
 //
-//        // Act & Assert
-//        mockMvc.perform(post("/auth/register")
-//                        .contentType(MediaType.APPLICATION_JSON)
-//                        .content("{\"firstName\":\"John\",\"lastName\":\"Doe\",\"email\":\"john.doe@example.com\",\"password\":\"password\",\"phone\":\"1234567890\"}"))
-//                .andExpect(status().isBadRequest())  // Adjusted to expect 400 Bad Request
-//                .andExpect(jsonPath("$.message").value("Registration unsuccessful"));
-//    }
-
-}
+//
+////    @Test
+////    void testRegisterUserFailsIfRequiredFieldsAreMissing() throws Exception {
+////        RegisterRequest request = new RegisterRequest();
+////        request.setEmail("john.doe@example.com");
+////        request.setPassword("password");
+////        request.setPhone("1234567890");
+////
+////        mockMvc.perform(post("/auth/register")
+////                        .contentType(MediaType.APPLICATION_JSON)
+////                        .content(objectMapper.writeValueAsString(request)))
+////                .andExpect(status().isBadRequest())
+////                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+////                .andExpect(jsonPath("$.status").value("Bad Request"))
+////                .andExpect(jsonPath("$.message").value("Registration unsuccessful"))
+////                .andExpect(jsonPath("$.statusCode").value(400));
+////    }
+//
+////    @Test
+////    void testRegisterUserFailsIfDuplicateEmail() throws Exception {
+////        // Arrange
+////        User existingUser = new User();
+////        existingUser.setEmail("john.doe@example.com");
+////        Mockito.when(userRepository.findUserByEmail("john.doe@example.com")).thenReturn(java.util.Optional.of(existingUser));
+////
+////        // Act & Assert
+////        mockMvc.perform(post("/auth/register")
+////                        .contentType(MediaType.APPLICATION_JSON)
+////                        .content("{\"firstName\":\"John\",\"lastName\":\"Doe\",\"email\":\"john.doe@example.com\",\"password\":\"password\",\"phone\":\"1234567890\"}"))
+////                .andExpect(status().isBadRequest())  // Adjusted to expect 400 Bad Request
+////                .andExpect(jsonPath("$.message").value("Registration unsuccessful"));
+////    }
+//
+//}
